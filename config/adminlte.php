4543a47bc1595b28b70146fbc8ff802ae0ef7e2a
bintangf <bintang.muhammad@binus.ac.id>
2021-06-30 22:08:22 +0700

    fix logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -46,7 +46,7 @@ return [
     */
 
     'logo' => 'SMK Islam Donomulyo',
-    'logo_img' => 'images/Logo.png',
+    'logo_img' => 'favicons/logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
