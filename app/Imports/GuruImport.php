bcdebfae9b39c68c23ee035d0ce6d5470010441f
bintangf <bintang.muhammad@binus.ac.id>
2021-08-03 21:01:13 +0700

    imports app

---
+++ app/Imports/GuruImport.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Imports;
+
+use App\Guru;
+use App\Mapel;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class GuruImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        $mapel = Mapel::where('nama_mapel', $row[3])->first();
+        $guru = Guru::create([
+            'nip' => $row[0],
+            'nama_guru' => $row[1],
+            'jk' => $row[2],
+        ]);
+        return $guru->mapel()->attach($mapel->id);
+    }
+}
---
+++ app/Imports/SiswaImport.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Imports;
+
+use App\Siswa;
+use App\Kelas;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class SiswaImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        $kelas = Kelas::where('nama_kelas', $row[3])->first();
+        return new Siswa([
+            'no_induk' => $row[0],
+            'nama_siswa' => $row[1],
+            'jk' => $row[2],
+            'kelas_id' => $kelas->id,
+        ]);
+    }
+}
