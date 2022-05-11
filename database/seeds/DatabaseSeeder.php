723064920bdc00cfdab47fce52b876f3af77954d
bintangf <bintang.muhammad@binus.ac.id>
2022-05-11 12:48:21 +0700

    tagihan seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Tagihan;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,5 +17,12 @@ class DatabaseSeeder extends Seeder
         $this->call(HariTableSeeder::class);
         $this->call(JurusansTableSeeder::class);
         $this->call(MapelSeeder::class);
+        //$this->call(TagihanSeeder::class);
+
+        Tagihan::create([
+            'nama' => 'SPP Semester Ganjil 2022/2023',
+            'jumlah' => '100000',
+            'keterangan' => 'Wajib',
+        ]);
     }
 }
