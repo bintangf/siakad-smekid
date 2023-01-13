1ca44d3dc65ecc982584a6e70c2c70a68d693ef1
Shift <shift@laravelshift.com>
2023-01-13 23:48:52 +0000

    Apply the Laravel code style

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\Guru;
 use App\Http\Controllers\Controller;
+use App\Models\Guru;
 use App\Models\Kelas;
-use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
