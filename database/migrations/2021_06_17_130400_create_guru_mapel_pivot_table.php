5de5402f79ea1dbddb52f86ff49bb65e1bc5e4a5
Shift <shift@laravelshift.com>
2023-01-13 23:49:50 +0000

    Adopt anonymous migrations

--- database/migrations/2021_06_17_130400_create_guru_mapel_pivot_table.php
+++ database/migrations/2021_06_17_130400_create_guru_mapel_pivot_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateGuruMapelPivotTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -30,4 +30,4 @@ class CreateGuruMapelPivotTable extends Migration
     {
         Schema::dropIfExists('guru_mapel');
     }
-}
+};
